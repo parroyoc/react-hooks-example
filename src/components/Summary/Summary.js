634dedacc9a55da471ee961cc21480ea9e4b72f5
PabloArroyo <pablo.x.arroyo@tescobank.com>
2018-12-09 08:29:21 +0000

    using useMemo for total price

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useMemo } from "react";
 import './Summary.css';
 
 export default function Summary({recipient, basketItems}) {
+  const totalPrice = useMemo(
+    () => {
+      const total = basketItems.reduce((sum, item) => sum + item.price, 0);
+      console.log('recalculating totalPrice', total);
+      return total;
+    },
+    [basketItems]
+  );
+
   return (
     <div className="summary">
       <h2>Summary:</h2>
@@ -13,6 +22,7 @@ export default function Summary({recipient, basketItems}) {
       {JSON.stringify(basketItems)}
 
       <h3>Review and pay:</h3>
+      <h4>{`Total: ${totalPrice}`}</h4>
       <button className="summary__submit" type="submit">pay</button>
     </div>
   );
